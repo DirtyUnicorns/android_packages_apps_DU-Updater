b57a37814242a5a9ea1c969e258f3ff13258acde
Bret Zamzow <mazwoz@gmail.com>
2016-07-24 00:17:39 +0200

    Add Parcelable and Serializable to File obj

--- app/src/main/java/com/dirtyunicorns/duupdater2/utils/File.java
+++ app/src/main/java/com/dirtyunicorns/duupdater2/utils/File.java
@@ -1,13 +1,37 @@
 package com.dirtyunicorns.duupdater2.utils;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
 /**
  * Created by mazwoz on 7/5/16.
  */
-public class File {
+public class File implements Parcelable, Serializable{
     private String FileName;
     private String FileSize;
     private String FileMD5;
     private String FildLink;
+
+    protected File(Parcel in) {
+        FileName = in.readString();
+        FileSize = in.readString();
+        FileMD5 = in.readString();
+        FildLink = in.readString();
+    }
+
+    public static final Creator<File> CREATOR = new Creator<File>() {
+        @Override
+        public File createFromParcel(Parcel in) {
+            return new File(in);
+        }
+
+        @Override
+        public File[] newArray(int size) {
+            return new File[size];
+        }
+    };
 
     public void SetFileName(String value) {
         FileName = value;
@@ -39,5 +63,18 @@ public class File {
 
     public String GetFileLink() {
         return FildLink;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(FileName);
+        dest.writeString(FileSize);
+        dest.writeString(FileMD5);
+        dest.writeString(FildLink);
     }
 }
