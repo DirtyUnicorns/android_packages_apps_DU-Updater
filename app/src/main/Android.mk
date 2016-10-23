0b3ba2f0676a09875dc3ed4242fec6e07d5c5132
Alex Cruz <mazdarider23@gmail.com>
2016-10-23 04:13:52 +0200

    Clean up / Get it compiling FINALLY!

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -7,31 +7,31 @@ LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_SRC_FILES := $(call all-java-files-under, java)
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/cardview/res \
-    frameworks/support/design/res \
-    frameworks/support/v7/recyclerview/res
+    $(support_library_root_dir)/v7/cardview/res \
+    $(support_library_root_dir)/v7/recyclerview/res \
+    $(support_library_root_dir)/v7/appcompat/res \
+    $(support_library_root_dir)/design/res
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.cardview
-LOCAL_AAPT_FLAGS += --extra-packages android.support.design
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
-LOCAL_AAPT_FLAGS += --extra-packages com.loopj.android.http
-LOCAL_AAPT_FLAGS += --extra-packages cz.msebera.android.httpclient
 
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat android-support-v7-cardview android-support-v13 android-support-design android-support-v7-recyclerview android-async-http httpclient
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages android.support.v7.appcompat \
+    --extra-packages android.support.v7.cardview \
+    --extra-packages android.support.v7.recyclerview \
+    --extra-packages android.support.design
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-common \
+    android-support-v13 \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-cardview \
+    android-support-v7-recyclerview \
+    android-support-design
 
 include $(BUILD_PACKAGE)
-
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := android-async-http:libs/android-async-http-1.4.9.jar \
-                                        httpclient:libs/httpclient-4.4.1.2.jar
-
-include $(BUILD_MULTI_PREBUILT)
-
